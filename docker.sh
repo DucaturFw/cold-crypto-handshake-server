33089474fbf374baecd0f50a968d6c81005016cc
zlumer <zlumer@gmail.com>
2019-11-04 20:25:16 +0300

    run docker as daemon

--- docker.sh
+++ docker.sh
@@ -2,4 +2,4 @@
 
 FIXDOC=`./fixdocker.sh`
 
-docker run -it --rm --name shake-server -p 127.0.0.1:3077:3077 -v "$FIXDOC":/usr/src/app -w /usr/src/app node:12.13.0-alpine /bin/sh -c "yarn && yarn start"
+docker run -d --rm --name shake-server -p 127.0.0.1:3077:3077 -v "$FIXDOC":/usr/src/app -w /usr/src/app node:12.13.0-alpine /bin/sh -c "yarn && yarn start"
