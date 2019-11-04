7279a9dfcceb8f4e22ff0fc554eebb6966221fe7
zlumer <zlumer@gmail.com>
2019-11-04 20:13:01 +0300

    shell script to run server inside docker

---
+++ docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+FIXDOC=`./fixdocker.sh`
+
+docker run -it --rm --name shake-server -p 127.0.0.1:3077:3077 -v "$FIXDOC":/usr/src/app -w /usr/src/app node:12.13.0-alpine /bin/sh -c "yarn && yarn start"
